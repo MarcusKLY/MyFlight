5f6d596beafd41c5393fef2736a7949aefcfe72e
MarcusKLY <50311376+MarcusKLY@users.noreply.github.com>
2026-03-23 22:21:47 +0200

    Initial Commit

---
+++ MyFlight/MyFlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFlightApp.swift
+//  MyFlight
+//
+//  Created by Kam Long Yin on 23/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyFlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
